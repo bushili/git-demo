997b310ee0587103b29743c4c823fb129dc2fbb5
test <asd2864801340@163.com>
2022-08-09 23:48:31 +0800

    test4

--- nacosregister/src/main/java/com/nacos/controller/testcontroller.java
+++ nacosregister/src/main/java/com/nacos/controller/testcontroller.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//asdasdasd
+//testtest
 @RestController
 @RefreshScope
 public class testcontroller {
