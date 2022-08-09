f551edcc42b14888d038046042e86e552249b482
test <asd2864801340@163.com>
2022-08-09 23:33:37 +0800

    testsecond

--- nacosregister/src/main/java/com/nacos/controller/testcontroller.java
+++ nacosregister/src/main/java/com/nacos/controller/testcontroller.java
@@ -4,6 +4,7 @@ package com.nacos.controller;
 import com.nacos.config.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
